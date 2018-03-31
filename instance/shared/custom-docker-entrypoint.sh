bd15c684d4485d532f048c06dd6c462ae2c4749a
Pierre Awaragi <pierre@awaragi.com>
2018-03-31 13:47:46 -0400

    added kb-authentication source code

--- instance/shared/custom-docker-entrypoint.sh
+++ instance/shared/custom-docker-entrypoint.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+
+cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
+cp -u /shared/*.ftl /opt/jboss/keycloak/themes/base/login
+
 /opt/jboss/docker-entrypoint.sh -b 0.0.0.0
