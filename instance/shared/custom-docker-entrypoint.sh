81f92b1d1c2fd516fd611afc50c51e9a61fb059a
Pierre Awaragi <pierre@awaragi.com>
2018-04-03 21:37:11 -0400

    centeralized all deployed artifacts in shared

--- instance/shared/custom-docker-entrypoint.sh
+++ instance/shared/custom-docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p /opt/jboss/keycloak/providers
+cp -u /shared/user-federation-1.0-SNAPSHOT.jar /opt/jboss/keycloak/providers
 cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
 cp -u /shared/*.ftl /opt/jboss/keycloak/themes/base/login
 
