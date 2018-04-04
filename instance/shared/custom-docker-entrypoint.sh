9c180c1fac9396bd51a01ef348724428c113937a
Pierre Awaragi <pierre.awaragi@cgi.com>
2018-04-04 15:20:27 -0400

    always display list of locales

--- instance/shared/custom-docker-entrypoint.sh
+++ instance/shared/custom-docker-entrypoint.sh
@@ -2,15 +2,16 @@
 
 mkdir -p /opt/jboss/keycloak/providers
 cp -u /shared/user-federation-1.0-SNAPSHOT.jar /opt/jboss/keycloak/providers
+
 cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
 cp /shared/*.ftl /opt/jboss/keycloak/themes/base/login
 
 mkdir -p /opt/jboss/keycloak/themes/wada/login/resources/img/
 mkdir -p /opt/jboss/keycloak/themes/wada/login/resources/css/
-cp /shared/resources/img/*.png /opt/jboss/keycloak/themes/wada/login/resources/img/
+cp /shared/resources/img/*.* /opt/jboss/keycloak/themes/wada/login/resources/img/
 cp /shared/resources/css/*.css /opt/jboss/keycloak/themes/wada/login/resources/css/
 cp /shared/theme.properties /opt/jboss/keycloak/themes/wada/login/
-cp /shared/*.ftl /opt/jboss/keycloak/themes/wada/login
+
 cp /shared/login_messages_en.properties /opt/jboss/keycloak/themes/wada/login/messages/messages_en.properties
 cp /shared/login_messages_ar.properties /opt/jboss/keycloak/themes/wada/login/messages/messages_ar.properties
 
