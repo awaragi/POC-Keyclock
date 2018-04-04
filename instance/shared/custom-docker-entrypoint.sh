e2c9872af539d849a43ad7e39d9efa31324585d0
Pierre Awaragi <pierre@awaragi.com>
2018-04-03 21:48:14 -0400

    added translated messages

--- instance/shared/custom-docker-entrypoint.sh
+++ instance/shared/custom-docker-entrypoint.sh
@@ -3,6 +3,8 @@
 mkdir -p /opt/jboss/keycloak/providers
 cp -u /shared/user-federation-1.0-SNAPSHOT.jar /opt/jboss/keycloak/providers
 cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
-cp -u /shared/*.ftl /opt/jboss/keycloak/themes/base/login
+cp /shared/*.ftl /opt/jboss/keycloak/themes/base/login
+cp /shared/login_messages_en.properties /opt/jboss/keycloak/themes/base/login/messages/messages_en.properties
+cp /shared/login_messages_ar.properties /opt/jboss/keycloak/themes/base/login/messages/messages_ar.properties
 
 /opt/jboss/docker-entrypoint.sh -b 0.0.0.0 --debug
