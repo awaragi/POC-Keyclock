aee088f0ba2f90d80d838ff58c68cc111abeb18f
Pierre Awaragi <pierre@awaragi.com>
2018-04-04 22:42:52 -0400

    organizing things for better management and proper inheritance of theme

--- instance/shared/custom-docker-entrypoint.sh
+++ instance/shared/custom-docker-entrypoint.sh
@@ -2,17 +2,8 @@
 
 mkdir -p /opt/jboss/keycloak/providers
 cp -u /shared/user-federation-1.0-SNAPSHOT.jar /opt/jboss/keycloak/providers
-
 cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
-cp /shared/*.ftl /opt/jboss/keycloak/themes/base/login
 
-mkdir -p /opt/jboss/keycloak/themes/wada/login/resources/img/
-mkdir -p /opt/jboss/keycloak/themes/wada/login/resources/css/
-cp /shared/resources/img/*.* /opt/jboss/keycloak/themes/wada/login/resources/img/
-cp /shared/resources/css/*.css /opt/jboss/keycloak/themes/wada/login/resources/css/
-cp /shared/theme.properties /opt/jboss/keycloak/themes/wada/login/
-
-cp /shared/login_messages_en.properties /opt/jboss/keycloak/themes/wada/login/messages/messages_en.properties
-cp /shared/login_messages_ar.properties /opt/jboss/keycloak/themes/wada/login/messages/messages_ar.properties
+cp -r /shared/themes/* /opt/jboss/keycloak/themes/
 
 /opt/jboss/docker-entrypoint.sh -b 0.0.0.0 --debug
