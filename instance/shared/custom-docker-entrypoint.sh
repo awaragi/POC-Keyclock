3f35978f070494d5b519d3cfdf58f6ba99e2e771
Pierre Awaragi <pierre.awaragi@cgi.com>
2018-04-05 13:32:29 -0400

    dockerfile to build image instead of custom launcher moving things around

--- instance/shared/custom-docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /opt/jboss/keycloak/providers
-cp -u /shared/user-federation-1.0-SNAPSHOT.jar /opt/jboss/keycloak/providers
-cp -u /shared/kb-authentication-1.0-SNAPSHOT.jar /opt/jboss/keycloak/standalone/deployments
-
-cp -r /shared/themes/* /opt/jboss/keycloak/themes/
-
-/opt/jboss/docker-entrypoint.sh -b 0.0.0.0 --debug
